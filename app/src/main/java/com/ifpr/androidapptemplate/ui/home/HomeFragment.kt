24670e9b57e8726074214e566ff299819b900626
Ericson Schmidt Bicalho <schcodes@gmail.com>
2025-11-20 20:45:41 -0300

    Move chat IA para aba de navegação e remove FAB

--- app/src/main/java/com/ifpr/androidapptemplate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ifpr/androidapptemplate/ui/home/HomeFragment.kt
@@ -43,7 +43,6 @@ import com.google.firebase.database.ValueEventListener
 import com.ifpr.androidapptemplate.R
 import com.ifpr.androidapptemplate.baseclasses.Item
 import com.ifpr.androidapptemplate.databinding.FragmentHomeBinding
-import com.ifpr.androidapptemplate.ui.ai.AiLogicActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -88,10 +87,6 @@ class HomeFragment : Fragment() {
         inicializaGerenciamentoLocalizacao()
         carregarAnalisesDoUsuario()
         setupLotterySection()
-        binding.fabAi.setOnClickListener {
-            startActivity(Intent(requireContext(), AiLogicActivity::class.java))
-        }
-
         return binding.root
     }
 
